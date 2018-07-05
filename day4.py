7eb71904db2024fd874d61b0e3b0c6f776031e08
Amit Tiwari <30861588+amit003@users.noreply.github.com>
2018-07-05 04:41:26 -0700

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,9 @@
+import random
+a=random.randint(1, 100)
+b=random.randint(1, 100)
+print('first number : ', a)
+print('second number : ', b)
+sum=a + b
+sub= a - b
+print('sum of two number : ' , sum)
+print('subtercation of two number : ', sub)
